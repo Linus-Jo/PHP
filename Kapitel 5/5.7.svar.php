fda2c66a2ec2270be439235a4173c68deddb66b3
AzureAD\LinusJohnsson <46134@gymnasium.ax>
2021-11-25 11:44:21 +0200

    lade till 5.7svar i kap 4

---
+++ Kapitel 5/5.7.svar.php
@@ -0,0 +1,11 @@
+<?php
+
+$favoritFarg=$_POST['favoritFarg'];
+
+?>
+<!DOCTYPE html>
+<html>
+	<body Style="background-color: <?=$favoritFarg; ?>">
+		<p style="background-color: White">ohhh vad vackert!!</p>
+	</body>
+</html>
