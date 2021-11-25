22ba82ff0222efbb9ee1607967d32f8bb8955e93
AzureAD\LinusJohnsson <46134@gymnasium.ax>
2021-11-25 11:53:11 +0200

    lade till teckenavkodning i Övrigt

---
+++ Övrigt/teckenavkodning.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Visa inmatade tecken</title>
+	</head>
+	<body>
+		<form method="POST">
+			Värde 1: <input type="text" name="text" size=50><br>
+			<input type="submit" value="Skicka">
+		</form>
+		<pre>
+<?php
+	$text=$_POST["text"];
+	echo "htmlentities:\n" . htmlentities($text);
+	echo "\nhtmlspecialchars:\n" . htmlspecialchars($text);
+	echo "\nurlencode:\n" . urlencode($text);
+	echo "\naddslashes:\n" . addslashes($text);
+?>
+		</pre>
+	</body>
+</html>
